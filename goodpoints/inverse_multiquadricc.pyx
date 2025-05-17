fe96c6b9b7ce34552d56cd73de870f2750c91142
Katsairyna Bokhan <kateqwerty001@gmail.com>
2025-05-17 12:58:43 +0200

    small fix - c is an array now

--- goodpoints/inverse_multiquadricc.pyx
+++ goodpoints/inverse_multiquadricc.pyx
@@ -10,41 +10,54 @@ np.import_array()
 @cython.initializedcheck(False)
 @cython.cdivision(True)
 cdef double inverse_multiquadric_kernel_two_points(const double[:] X1,
-                                                      const double[:] X2,
-                                                      const double[:] c) noexcept nogil:
+                                                    const double[:] X2,
+                                                    const double[:] c_params) noexcept nogil:
     """
-    Computes the inverse multiquadratic kernel 
-    k(X1, X2) = 1 / sqrt(c + ||X1 - X2||^2)
+    Computes a sum of inverse multiquadratic kernels:
+    k(X1, X2) = sum_j 1 / sqrt(c_j + ||X1 - X2||^2)
 
     Args:
       X1: array of size d
       X2: array of size d
-      c: array of size 1, scalar > 0 stored at c[0]
+      c_params: array of size num_kernels, each c_j > 0 
     """
     cdef long d = X1.shape[0]
+    cdef long num_kernels = c_params.shape[0]
+    
     cdef double sq_dist = 0.
     cdef double diff
     cdef long j
-
+    
     for j in range(d):
         diff = X1[j] - X2[j]
         sq_dist += diff * diff
-        
-    return 1. / sqrt(c[0] + sq_dist)
 
+    cdef double kernel_sum = 0.
+    for j in range(num_kernels):
+        kernel_sum += 1.0 / sqrt(c_params[j] + sq_dist)
+    
+    return kernel_sum
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.initializedcheck(False)
 @cython.cdivision(True)
 cdef double inverse_multiquadric_kernel_one_point(const double[:] X1,
-                                                     const double[:] c) noexcept nogil:
+                                                  const double[:] c_params) noexcept nogil:
     """
-    Computes the inverse multiquadratic kernel between X1 and itself:
-    k(X1, X1) = 1 / sqrt(c[0])
+    Computes the sum of inverse multiquadratic kernels between X1 and itself:
+    k(X1, X1) = sum_j 1 / sqrt(c_j)
 
     Args:
-      X1: array of size d
-      c: array of size 1, scalar > 0 stored at c[0]
+      X1: array of size d (ignored, kept for consistency)
+      c_params: array of kernel parameters, each > 0
     """
-    return 1. / sqrt(c[0])
+    cdef long num_kernels = c_params.shape[0]
+    cdef long j
+    cdef double kernel_sum = 0.
+
+    for j in range(num_kernels):
+        kernel_sum += 1.0 / sqrt(c_params[j])
+
+    return kernel_sum
+
