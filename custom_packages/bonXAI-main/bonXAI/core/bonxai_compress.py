9b76703eefbdfea7f6871b6aadb865e6578c904c
Katsairyna Bokhan <kateqwerty001@gmail.com>
2025-11-13 18:26:31 +0300

    bonxai_compress fixed

--- custom_packages/bonXAI-main/bonXAI/core/bonxai_compress.py
+++ custom_packages/bonXAI-main/bonXAI/core/bonxai_compress.py
@@ -3,8 +3,14 @@ from numpy.random import SeedSequence
 from goodpoints import compressc, kt, compress
 import math
 
+def largest_power_of_four(n):
+    """
+    Returns largest power of four less than or equal to n
+    """
+    return 4**( (n.bit_length() - 1 )//2)
+
 def compute_m(n, target_size, g, num_bins):
-    n_prime = num_bins * compress.largest_power_of_four(math.floor(n / num_bins))
+    n_prime = num_bins * largest_power_of_four(math.floor(n / num_bins))
     compress_coreset_size = min(2**g * math.sqrt(n_prime * num_bins), n_prime)
     if target_size > compress_coreset_size:
         raise ValueError("target_size is too large for bonxai_compress with set num_bins and g parameters")
@@ -20,7 +26,7 @@ def bonxai_compress(X, kernel_type, k_params=np.ones(1), g=4, num_bins=32,
                 target_size=None, delta=0.5, seed=None, mean0=False):
     n = X.shape[0]
 
-    nearest_pow_four = compress.largest_power_of_four(n)
+    nearest_pow_four = largest_power_of_four(n)
     sqrt_nearest_pow_four = int(math.sqrt(nearest_pow_four))
 
     if target_size>n or target_size<1:
@@ -32,11 +38,17 @@ def bonxai_compress(X, kernel_type, k_params=np.ones(1), g=4, num_bins=32,
             X[input_indices], kernel_type, k_params=k_params, g=g, 
             num_bins=num_bins, target_size=target_size, delta=delta, seed=seed, mean0=mean0) ]
     
-    if target_size == None or target_size == int(math.sqrt(nearest_pow_four)):
-        return compress.compresspp_kt(X, kernel_type, k_params, g, num_bins, delta, seed, mean0)
-    
     m = compute_m(n, target_size, g, num_bins)
     log2_target_size = int(math.log2(target_size))
+
+    if sqrt_nearest_pow_four == target_size:
+        if log2_target_size <= m:
+            print("No compress")
+            K = np.empty((n, n))
+            compressc.compute_K(X, np.arange(n, dtype=int), kernel_type, k_params, 
+                                K)
+            
+            return kt.thin_K(K, K, log2_target_size, delta=delta, seed=seed, mean0=mean0)
         
     thin_frac = m / (m + (2**m) * (log2_target_size - m))
     thin_delta = delta * thin_frac
